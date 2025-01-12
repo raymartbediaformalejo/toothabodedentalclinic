f1929b6750ca92200ee6767c25d4449ebbfad691
Raymart <100412726+raymartbediaformalejo@users.noreply.github.com>
2025-01-12 21:59:04 +0800

    Update tailwind.config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+/*eslint-env node*/
+
+module.exports = {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
